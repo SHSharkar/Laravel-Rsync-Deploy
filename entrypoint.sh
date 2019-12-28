3e783b2becda4a98c9c3446967e10fdcf1f3466b
Sazzad Hossain Sharkar <sh@sharkar.net>
2019-12-28 15:10:39 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ chmod 600 "$SSHPATH/key"
 SERVER_DEPLOY_STRING="$REMOTE_USER@$REMOTE_HOST:$TARGET_DIRECTORY"
 
 # Run Rsync synchronization
-sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_HOST_PORT' $GITHUB_WORKSPACE $SERVER_DEPLOY_STRING"
+sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_HOST_PORT' . $SERVER_DEPLOY_STRING"
